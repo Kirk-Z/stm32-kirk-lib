49bbbbb4d331cdddcef886161023b0bf06d6e1e3
Kirk <48848269+Kirk-Z@users.noreply.github.com>
2021-01-07 18:46:57 +0800

    Device Layer - LED
    General controls for typical LEDs.

---
+++ Klib_Device_Layer/kdl_led.c
@@ -0,0 +1,141 @@
+/**
+  ******************************************************************************
+  * @date    Jan 7, 2021
+  * @file    kdl_led.c
+  * @author  Kirk_Z
+  * @name    Kefan Zheng
+  * @brief   Source file of Device LED
+  * @email   kirk_z@yeah.net
+  ******************************************************************************
+  * @attention
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "kdl_led.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+
+/**
+  * @brief  Initialize the LED
+  * @param  dled  pointer of the led
+  * @param  port  GPIO port
+  * @param  pin  GPIO pin number
+  * @param  stat  indicate whether low/high level associates the LED on
+  *
+  * @retval KDL_State_t KDL Status
+  */
+KDL_State_t KDL_LED_Init(KDL_LED_t* dled, GPIO_TypeDef* port, uint16_t pin, KDL_LED_State stat)
+{
+	if(dled == NULL)
+		return KDL_ERROR;
+
+	dled->Port = port;
+	dled->Pin = pin;
+	dled->Onstate = stat;
+
+	KDL_LED_ResetCheck(dled);
+
+	return KDL_OK;
+}
+
+/**
+  * @brief  Turn on the LED
+  * @param  dled  pointer of the led
+  *
+  * @retval KDL_State_t KDL Status
+  */
+KDL_State_t KDL_LED_TurnOn(KDL_LED_t* dled)
+{
+	if(!__IS_KDL_LED_T(dled))
+		return KDL_ERROR;
+
+	HAL_GPIO_WritePin(dled->Port, dled->Pin, (GPIO_PinState)(dled->Onstate));
+
+	return KDL_OK;
+}
+
+/**
+  * @brief  Turn off the LED
+  * @param  dled  pointer of the led
+  *
+  * @retval KDL_State_t KDL Status
+  */
+KDL_State_t KDL_LED_TurnOff(KDL_LED_t* dled)
+{
+	if(!__IS_KDL_LED_T(dled))
+		return KDL_ERROR;
+
+	HAL_GPIO_WritePin(dled->Port, dled->Pin, (GPIO_PinState)(!dled->Onstate));
+
+	return KDL_OK;
+}
+
+/**
+  * @brief  Toggle the LED
+  * @param  dled  pointer of the led
+  *
+  * @retval KDL_State_t KDL Status
+  */
+KDL_State_t KDL_LED_Toggle(KDL_LED_t* dled)
+{
+	if(!__IS_KDL_LED_T(dled))
+		return KDL_ERROR;
+
+	HAL_GPIO_TogglePin(dled->Port, dled->Pin);
+
+	return KDL_OK;
+}
+
+/**
+  * @brief  Turn on/off the LED
+  * @param  dled  pointer of the led
+  * @param  stat  on/off
+  *
+  * @retval KDL_State_t KDL Status
+  */
+KDL_State_t KDL_LED_Turn(KDL_LED_t* dled, KDL_LED_State stat)
+{
+	if(!__IS_KDL_LED_T(dled))
+		return KDL_ERROR;
+
+	HAL_GPIO_WritePin(dled->Port, dled->Pin, (GPIO_PinState)(!(stat ^ dled->Onstate)));
+
+	return KDL_OK;
+}
+
+/**
+  * @brief  Check whether it is legal LED
+  * @param  dled  pointer of the led
+  *
+  * @retval KDL_State_t KDL Status
+  */
+KDL_State_t KDL_LED_Check(KDL_LED_t* dled)
+{
+	return dled->check == (uint32_t)dled ? KDL_OK : KDL_ERROR;
+}
+
+/**
+  * @brief  Reset the check value of the LED
+  * @param  dled  pointer of the led
+  *
+  * @retval KDL_State_t KDL Status
+  */
+KDL_State_t KDL_LED_ResetCheck(KDL_LED_t* dled)
+{
+	if(dled == NULL)
+		return ERROR;
+
+	dled->check = (uint32_t)dled;
+
+	return KDL_OK;
+}
+
+
---
+++ Klib_Device_Layer/kdl_led.h
@@ -0,0 +1,84 @@
+/**
+  ******************************************************************************
+  * @date    Jan 7, 2021
+  * @file    kdl_led.h
+  * @author  Kirk_Z
+  * @name    Kefan Zheng
+  * @brief   Header file of Device LED
+  * @email   kirk_z@yeah.net
+  ******************************************************************************
+  * @attention
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef KDL_LED_H_
+#define KDL_LED_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "kdl.h"
+
+/* Exported types ------------------------------------------------------------*/
+
+/**
+  * @brief  enum of LED on status
+  */
+typedef enum {
+	KDL_LED_ONSTATE_LOW = 0,
+	KDL_LED_ONSTATE_HIGH
+} KDL_LED_OnState;
+
+/**
+  * @brief  enum of LED status
+  */
+typedef enum {
+	KDL_LED_OFF = 0,
+	KDL_LED_ON
+} KDL_LED_State;
+
+/**
+  * @brief  type of LED
+  */
+typedef struct _KDL_LED_t {
+
+	GPIO_TypeDef* Port;
+	uint16_t Pin;
+
+	KDL_LED_State Onstate;
+
+	uint32_t check;
+
+} KDL_LED_t;
+
+/* Exported constants --------------------------------------------------------*/
+/* Exported macro ------------------------------------------------------------*/
+/* Private constants ---------------------------------------------------------*/
+/* Private macros ------------------------------------------------------------*/
+#define __IS_KDL_LED_T(__HANDLE__) (KDL_OK == KDL_LED_Check(__HANDLE__))
+/* Exported functions --------------------------------------------------------*/
+
+/* Configuration functions ****************************************************/
+KDL_State_t KDL_LED_Init(KDL_LED_t* dled, GPIO_TypeDef* port, uint16_t pin, KDL_LED_State stat);
+KDL_State_t KDL_LED_Check(KDL_LED_t* dled);
+KDL_State_t KDL_LED_ResetCheck(KDL_LED_t* dled);
+
+/* IO operation functions *****************************************************/
+KDL_State_t KDL_LED_TurnOn(KDL_LED_t* dled);
+KDL_State_t KDL_LED_TurnOff(KDL_LED_t* dled);
+KDL_State_t KDL_LED_Toggle(KDL_LED_t* dled);
+KDL_State_t KDL_LED_Turn(KDL_LED_t* dled, KDL_LED_State stat);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* KDL_LED_H_ */
+
+/************************ (C) COPYRIGHT kirkz.tech *****END OF FILE****/
